e3791ca107062012ef255b71511f0835c22a19bd
lxw <1600920733@qq.com>
2018-01-11 14:07:34 +0800

    'shangchuan'

---
+++ get.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php echo $_GET['id']?>
+<input type="text" value="">
+</body>
+</html>
